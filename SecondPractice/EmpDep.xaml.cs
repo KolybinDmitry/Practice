fab0df4e64212f978a9a7924fab8f6bde18b42d9
KolybinDmitry <isip_d.v.kolybin@mpt.ru>
2023-03-29 20:20:03 +0300

    ss

---
+++ SecondPractice/EmpDep.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+using SecondPractice.TestDbDataSetTableAdapters;
+
+namespace SecondPractice
+{
+    /// <summary>
+    /// Логика взаимодействия для EmpDep.xaml
+    /// </summary>
+    public partial class EmpDep : Window
+    {
+        emp_deptTableAdapter empdep = new emp_deptTableAdapter();
+        employeesTableAdapter Employees = new employeesTableAdapter();
+        departmentsTableAdapter departments = new departmentsTableAdapter();
+
+
+        public EmpDep()
+        {
+            InitializeComponent();
+            EmpDepDataGrid.ItemsSource = empdep.GetData();
+            EmployeeCbx.ItemsSource=Employees.GetData();
+            EmployeeCbx.DisplayMemberPath = "emp_name";
+            EmployeeCbx.SelectedValuePath = "emp_id";
+            DepartmentCbx.ItemsSource = departments.GetData();
+            DepartmentCbx.DisplayMemberPath = "dept_name";
+            DepartmentCbx.SelectedValuePath = "dept_id";
+
+        }
+
+
+
+        private void Bt1_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            object id = (EmpDepDataGrid.SelectedItem as DataRowView).Row[0];
+            empdep.DeleteQuery(Convert.ToInt32(id));
+            EmpDepDataGrid.ItemsSource = empdep.GetData();
+        }
+
+        private void changeButton_Click(object sender, RoutedEventArgs e)
+        {
+            object id = (EmpDepDataGrid.SelectedItem as DataRowView).Row[0];
+            object id1 = (EmpDepDataGrid.SelectedItem as DataRowView).Row[1];
+            object id2 = (EmployeeCbx.SelectedItem as DataRowView).Row[0];
+            object id3 = (DepartmentCbx.SelectedItem as DataRowView).Row[0];
+            empdep.UpdateQuery(Convert.ToInt32(id2), Convert.ToInt32(id3), Convert.ToInt32(id), Convert.ToInt32(id1));
+            EmpDepDataGrid.ItemsSource = empdep.GetData();
+        }
+        private void EmpDepDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (EmpDepDataGrid.SelectedItem as DataRowView != null)
+            {
+                EmployeeCbx.SelectedValue = Convert.ToInt32((EmpDepDataGrid.SelectedItem as DataRowView).Row[0]);
+                DepartmentCbx.SelectedValue = Convert.ToInt32((EmpDepDataGrid.SelectedItem as DataRowView).Row[1]);
+            }
+           }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+            object id = (EmployeeCbx.SelectedItem as DataRowView).Row[0];
+            object id1 = (DepartmentCbx.SelectedItem as DataRowView).Row[0];
+            empdep.InsertQuery(Convert.ToInt32(id), Convert.ToInt32(id1));
+           EmpDepDataGrid.ItemsSource = empdep.GetData();
+        }
+    }
+}
---
+++ SecondPractice/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SecondPractice.TestDbDataSetTableAdapters;
+
+namespace SecondPractice
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        employeesTableAdapter Employees = new employeesTableAdapter();
+        public MainWindow()
+        {
+            InitializeComponent();
+            EmployeesDataGrid.ItemsSource = Employees.GetData();
+        }
+
+        private void Bt_Click(object sender, RoutedEventArgs e)
+        {
+            Employees.InsertQuery(Convert.ToInt32(NameTbx.Text), Convert.ToString(NameTbx1.Text), Convert.ToString(NameTbx2.Text), Convert.ToInt32(NameTbx3.Text));
+
+            EmployeesDataGrid.ItemsSource = Employees.GetData();
+        }
+
+        private void Bt1_Click(object sender, RoutedEventArgs e)
+        {
+           EmpDep  window1 = new EmpDep();
+            window1.ShowDialog();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            object id = (EmployeesDataGrid.SelectedItem as DataRowView).Row[0];
+            Employees.DeleteQuery(Convert.ToInt32(id));
+            EmployeesDataGrid.ItemsSource = Employees.GetData();
+        }
+
+        private void changeButton_Click(object sender, RoutedEventArgs e)
+        {
+            object id = (EmployeesDataGrid.SelectedItem as DataRowView).Row[0];
+            Employees.UpdateQuery(Convert.ToInt32(NameTbx.Text), NameTbx1.Text, NameTbx2.Text, Convert.ToInt32(NameTbx3.Text),Convert.ToInt32(id));
+            EmployeesDataGrid.ItemsSource = Employees.GetData();
+        }
+        private void EmployeesDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (EmployeesDataGrid.SelectedItem as DataRowView != null)
+            {
+                NameTbx.Text = Convert.ToString((EmployeesDataGrid.SelectedItem as DataRowView).Row[0]);
+                NameTbx1.Text = Convert.ToString((EmployeesDataGrid.SelectedItem as DataRowView).Row[1]);
+                NameTbx2.Text = Convert.ToString((EmployeesDataGrid.SelectedItem as DataRowView).Row[2]);
+                NameTbx3.Text = Convert.ToString((EmployeesDataGrid.SelectedItem as DataRowView).Row[3]);
+            }
+        }
+    }
+}
